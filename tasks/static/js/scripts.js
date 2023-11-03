16f5e3a687188842f2137e4032dba2e621ae72e1
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-11-03 19:23:05 -0300

    Adicionando JS

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,24 @@
-console.log("funcionou!");
+document.addEventListener('DOMContentLoaded', function() {
+
+    var deleteBtns = document.querySelectorAll('.delete-btn');
+    var searchBtn = document.getElementById('search-btn');
+    var searchForm = document.getElementById('search-form');
+    
+    deleteBtns.forEach(function(deleteBtn) {
+        deleteBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            var delLink = this.getAttribute('href');
+            var result = confirm('Quer deletar esta tarefa?');
+            if(result) {
+                window.location.href = delLink;
+            }
+        });
+    });
+
+    if (searchBtn) {
+        searchBtn.addEventListener('click', function() {
+            searchForm.submit();
+        });
+    }
+
+});
