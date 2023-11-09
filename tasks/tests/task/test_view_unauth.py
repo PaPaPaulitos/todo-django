0a67d2eabae0878a166585cfde7c73f36ebe935d
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-11-09 02:18:21 -0300

    Adicionando testes na views de task sem estar autenticado

---
+++ tasks/tests/task/test_view_unauth.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from faker import Faker
+
+fake = Faker()
+
+class TaskTestCase(TestCase):
+    def test_status_code_302_redirect_if_you_not_authenticated(self):
+        response = self.client.get(reverse('task-view', args=[1]))
+        self.assertEquals(response.status_code, 302)
+
+    def test_redirect_if_you_not_authenticated(self):
+        response = self.client.get(reverse('task-view',args=[1]))
+        self.assertRedirects(response, '/accounts/login/?next=/task/1')
+
+    
+    def test_redirect_if_you_not_authenticated_template(self):
+        response = self.client.get(reverse('task-view',args=[1]), follow=True)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+
+    
