6d0bc242a70d33aa56934c3c2f018e96e3ba9339
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-11-09 02:06:03 -0300

    Atualizando testes na area de autenticacao

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 from faker import Faker
@@ -6,7 +7,6 @@ from faker import Faker
 fake = Faker()
 
 class TaskTestCase(TestCase):
-    
     def test_status_code_200_hello_world(self):
         response = self.client.get(reverse('hello-world'))
         self.assertEquals(response.status_code, 200)
@@ -25,9 +25,9 @@ class TaskTestCase(TestCase):
         self.assertRedirects(response, '/accounts/login/?next=/')
 
     
-    """
     def test_redirect_if_you_not_authenticated_template(self):
-        response = self.client.get(reverse('tasks-list'))
+        response = self.client.get(reverse('tasks-list'), follow=True)
         self.assertTemplateUsed(response, 'registration/login.html')
-    """
+
+
     
