6255f7acc4bd4c970abad41d15233281af547f79
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-11-09 14:14:06 -0300

    Test newtask

---
+++ tasks/tests/task-newtask/test_views_auth.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from faker import Faker
+
+from tasks.models import Task
+
+fake = Faker()
+
+class TaskTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('john', 'john@example.com', 'johnpassword')
+        self.task = Task.objects.create(title="Test Task", description="Test Description", user=self.user)
+    
+    def test_status_code_200_after_authentication(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('new-task'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used_after_authentication(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('new-task'))
+        self.assertTemplateUsed(response, 'tasks/addtask.html')
+
+
+    
