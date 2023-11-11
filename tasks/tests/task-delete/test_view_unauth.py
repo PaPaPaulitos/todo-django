ba609ce51307af6870e7c18dcd71e42dd761f515
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-11-11 16:57:13 -0300

    Adicionando Tasks Test

--- tasks/tests/task-delete/test_view_unauth.py
+++ tasks/tests/task-delete/test_view_unauth.py
@@ -8,14 +8,14 @@ fake = Faker()
 
 class TaskTestCase(TestCase):
     def test_status_code_302_redirect_if_you_not_authenticated(self):
-        response = self.client.get(reverse('delete-task', args=[1]))
+        response = self.client.get(reverse('edit-task', args=[1]))
         self.assertEquals(response.status_code, 302)
 
     def test_redirect_if_you_not_authenticated(self):
-        response = self.client.get(reverse('delete-task',args=[1]))
-        self.assertRedirects(response, '/accounts/login/?next=/delete/1')
+        response = self.client.get(reverse('edit-task',args=[1]))
+        self.assertRedirects(response, '/accounts/login/?next=/edit/1')
 
     
     def test_redirect_if_you_not_authenticated_template(self):
-        response = self.client.get(reverse('delete-task',args=[1]), follow=True)
+        response = self.client.get(reverse('edit-task',args=[1]), follow=True)
         self.assertTemplateUsed(response, 'registration/login.html')
